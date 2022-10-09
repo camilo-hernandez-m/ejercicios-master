1a70a4284b81aa8b30b03e22fa0c099d55db4f7b
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:24:07 -0500

    ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,5 +1,5 @@
 addEventListener ("DOMContentLoaded", ()=>{
-//7. Escribir un programa que calcule el volumen de una esfera: Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
+//7. Escribir un programa que c2alcule el volumen de una esfera: Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
 let div=Number(4/3)
 // ${match.PI}
 
