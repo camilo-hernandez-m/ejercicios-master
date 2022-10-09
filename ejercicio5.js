7e1a4477add5d046330c80e769006e81e403c5e7
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:23:50 -0500

    ejercicio5.js

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,6 +1,6 @@
 addEventListener("DOMContentLoaded", ()=>{
     // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
+    // Longitud de la circunferencia = 2 * 2PI * radio
     // Área de la circunferencia = PI * radio˄2
 
 
