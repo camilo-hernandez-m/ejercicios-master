19dd19ed3e596d7d805a19519d4b0eda56982a52
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:23:59 -0500

    ejercicio6.js

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar el resultado en metros/segundo. Velocidad = espacio/tiempo.
+    //Escribir un programa que calcul2e la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar el resultado en metros/segundo. Velocidad = espacio/tiempo.
     let mets=2*1000
     let segs=60*5
     let tiempo=5
