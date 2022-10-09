41b4c605015cf6b778d8375d1aea53b680b44f1e
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:25:39 -0500

    ejercicio19.js

--- ejercicio19.js
+++ ejercicio19.js
@@ -1,7 +1,7 @@
 addEventListener ("DOMContentLoaded", ()=>{
     //19. Un estudiante realiza cuatro exámenes. Realizar el pseudocódigo que representen el algoritmo correspondiente para obtener el promedio de las calificaciones obtenidas. las calificaciones van 1 a 5 puntos.
 
-    //19 CUATRO EXAMENES
+    //19 CUATRO2 EXAMENES
     //Datos de entrada
     let a=Number(prompt("ingrese la calificacion 1: "));
     let b=Number(prompt("ingrese la calificacion 2: "));
