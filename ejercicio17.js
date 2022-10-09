2e647101132ac2df98244c4798f82884bada6613
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:25:24 -0500

    ejercicio17.js

--- ejercicio17.js
+++ ejercicio17.js
@@ -1,7 +1,7 @@
 addEventListener ("DOMContentLoaded", ()=>{
     //17. Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el tiempo y de un producto a otro, este dato se lee por teclado.
 
-    //17 VALOR DEL COSTO DEL PRODUCTO
+    //17 VALOR DE2L COSTO DEL PRODUCTO
     //Datos de entrada
     console.log("ingrese el nombre del primer producto:")
     let p1=prompt()
