321c974eb15abdf29725af2e33e7bc6b078b501b
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:25:16 -0500

    ejercicio16.js

--- ejercicio16.js
+++ ejercicio16.js
@@ -2,7 +2,7 @@ addEventListener ("DOMContentLoaded", ()=>{
     //16. Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación secompone de los siguientes porcentajes: 55% del promedio final de sus calificaciones de los tres(3) parciales. 30% de la calificación examen final y 15% de la calificación trabajo final.
 
     //16 FINAL DE LA MATERIA DE ALGORITMOS
-    // datos de entrada
+    // datos de en2trada
     let a=Number(prompt("ingrese la calificacion 1: "))
     let b=Number(prompt("ingrese la calificacion 2: "))
     let c=Number(prompt("ingrese la calificacion 3: "))
