38c89c475475cd1cc051144e0b3176cbfb1c421c
PedroAntonioSanchezYepesADSO1 <112569944+PedroAntonioSanchezYepesADSO1@users.noreply.github.com>
2022-10-09 12:20:07 -0500

    ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,6 +1,6 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-    // 2. Escribir un programa que calcule el área de un rectángulo:
+    // 2. Escribir un programa que calcule el área de un rectá1ngulo:
     // lado1 = 3 lado2 = 4 área del rectángulo= lado1 * lado2
     let lado1 = Number(prompt("Ingrese el lado 1", 3));
     let lado2 = Number(prompt("Ingrese el lado 2", 4));
